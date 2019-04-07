9b97ffee890b816d3a42bd8637de8e29dff46962
Ana <anangoclu@gmail.com>
2019-04-07 14:38:11 -0700

    fix mistake from last commit

--- skincare_selfcare.js
+++ skincare_selfcare.js
@@ -3,10 +3,10 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  // .use(express.static(path.join(__dirname, 'public')))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  .get('/', (req, res) => res.json({ info: 'Node.js, Express, and Postgres API' }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // const express = require('express');
