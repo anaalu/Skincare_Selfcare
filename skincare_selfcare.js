52a700890bf59998de815f2c99de33c7c81f087b
Ana <anangoclu@gmail.com>
2019-04-07 14:25:06 -0700

    change code to debug heroku crash

--- skincare_selfcare.js
+++ skincare_selfcare.js
@@ -1,20 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
-// const db = require('./src/queries')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// const express = require('express');
+// const bodyParser = require('body-parser')
+// const app = express()
+// const port = process.env.PORT || 3000
+// // const db = require('./src/queries')
+
+// app.use(bodyParser.json())
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// )
 
 // root endpoint
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+// app.get('/', (request, response) => {
+//   response.json({ info: 'Node.js, Express, and Postgres API' })
+// })
 
 // app.get('/users', db.getUsers)
 // app.get('/users/:id', db.getUserById)
@@ -22,9 +33,9 @@ app.get('/', (request, response) => {
 // app.put('/users/:id', db.updateUser)
 // app.delete('/users/:id', db.deleteUser)
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}.`)
+// })
 
 // const http = require('http');
 // const hostname = '127.0.0.1';
