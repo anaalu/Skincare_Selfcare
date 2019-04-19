1dc6302fa2c1073d351314b193e8f5f0221d1042
Ana <anangoclu@gmail.com>
2019-04-19 16:49:53 -0700

    add styling for header

--- skincare_selfcare.js
+++ skincare_selfcare.js
@@ -1,54 +1,52 @@
+// const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  // connectionString: process.env.DATABASE_URL,
+  // ssl: true
+  
+    user: 'me',
+    host: 'localhost',
+    database: 'testdb',
+    password: 'password',
+    port: 5432,
+  
+});
+
 express()
-  // .use(express.static(path.join(__dirname, 'public')))
-  // .set('views', path.join(__dirname, 'views'))
-  // .set('view engine', 'ejs')
-  .get('/', (req, res) => res.json({ info: 'Node.js, Express, and Postgres API' }))
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/login', (req, res) => res.render('pages/login'))
+  .get('/db', async (req, res) => {
+    try {
+      const results = await getUsers();
+      res.render('pages/db', results );
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// const express = require('express');
-// const bodyParser = require('body-parser')
-// const app = express()
-// const port = process.env.PORT || 3000
-// // const db = require('./src/queries')
 
-// app.use(bodyParser.json())
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// )
+showTimes = () => {
+  let result = ''
+  const times = process.env.TIMES || 5
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  return result;
+}
 
-// root endpoint
-// app.get('/', (request, response) => {
-//   response.json({ info: 'Node.js, Express, and Postgres API' })
-// })
-
-// app.get('/users', db.getUsers)
-// app.get('/users/:id', db.getUserById)
-// app.post('/users', db.createUser)
-// app.put('/users/:id', db.updateUser)
-// app.delete('/users/:id', db.deleteUser)
-
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}.`)
-// })
-
-// const http = require('http');
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-// 'node http_server.js' to start server
+getUsers = async () => {
+  const client = await pool.connect()
+  const result = await client.query('SELECT * FROM test_table');
+  const results = { 'results': (result) ? result.rows : null};
+  client.release();
+  return results;
+}
